fce6e4e74baa0a6927d2fad2cb26ae4570eabe20
rafaelpush <rafaliicontact@gmail.com>
2025-11-22 02:31:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import createPayment from "./create-payment.js";
+import webhook from "./webhook.js";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors({
+  origin: "https://webeconomia.vercel.app/valores.html" // frontend hospedado na Vercel
+}));
+
+// Rotas
+app.post("/create-payment", createPayment);
+app.post("/webhook", webhook);
+
+// Start do servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
