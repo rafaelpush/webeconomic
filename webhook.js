fdb28d12e7fa3e09890f0e24231102954cb786b0
rafaelpush <rafaliicontact@gmail.com>
2025-11-22 02:29:04 -0300

    Create webhook.js

---
+++ webhook.js
@@ -0,0 +1,41 @@
+import mercadopago from "mercadopago";
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN)),
+  });
+}
+
+mercadopago.configure({
+  access_token: process.env.MP_ACCESS_TOKEN,
+});
+
+export default async function handler(req, res) {
+  try {
+    const data = req.body;
+
+    if (data.type !== "payment") {
+      return res.status(200).send("ignored");
+    }
+
+    const paymentId = data.data.id;
+    const info = await mercadopago.payment.findById(paymentId);
+
+    const payment = info.body;
+
+    if (payment.status === "approved") {
+      const [uid, plano] = payment.external_reference.split("|");
+
+      await admin.firestore().collection("users").doc(uid).set(
+        { plan: plano },
+        { merge: true }
+      );
+    }
+
+    res.status(200).send("ok");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("erro");
+  }
+}
