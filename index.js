ff7b736d826eadc134a48d088785b3341e7f93e1
rafaelpush <rafaliicontact@gmail.com>
2025-11-29 22:15:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// ===============================
-// IMPORTA ARQUIVOS DA PASTA /api
-// ===============================
+// Rotas
 import vincularRouter from "./api/vincular.js";
 
-// ===============================
-// ROTAS
-// ===============================
-app.use("/api", vincularRouter);   // Google login + Discord OAuth
+app.use("/api", vincularRouter);
 
-// ===============================
-// PORTA DO SERVIDOR
-// ===============================
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
-});
+app.listen(PORT, () => console.log("Backend ativo na porta " + PORT));
