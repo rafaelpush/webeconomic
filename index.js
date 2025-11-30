2ba2002e2dadb2fdb6b65872a09e63e47847edf8
rafaelpush <rafaliicontact@gmail.com>
2025-11-30 16:43:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import vincularRouter from "./api/vincular.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// --- AGORA A ROTA BASE É /api/vincular ---
-app.use("/api/vincular", vincularRouter);
 
 app.get("/", (req, res) => {
   res.send("API WebEconomia ativa!");
