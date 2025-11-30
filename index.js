0ee037db3e8281a6d288b3f6c1ffbfe592b12874
rafaelpush <rafaliicontact@gmail.com>
2025-11-29 22:25:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import discordRouter from "./api/discord.js";
+import vincularRouter from "./api/vincular.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", discordRouter);   
+app.use("/api", vincularRouter);
 
 app.get("/", (req, res) => {
   res.send("API WebEconomia ativa!");
